c584832aa987d98519f7e3744b8efe7eb3390d6b
Lailson Da Silva <Lailsonkazumi@gmail.com>
2021-10-15 20:58:54 -0300

    adicionando função para buscar infromações dos ceps

--- web/forms/diarista_form.py
+++ web/forms/diarista_form.py
@@ -3,17 +3,20 @@ from ..models import Diarista
 
 
 class DiaristaForm(forms.ModelForm):
+    #aplicando mascara nos campos necessários
+
     cpf = forms.CharField(widget=forms.TextInput(attrs={'data-mask': "000.000.000-00"}))
     cep = forms.CharField(widget=forms.TextInput(attrs={'data-mask': "00000-000"}))
     telefone = forms.CharField(widget=forms.TextInput(attrs={'data-mask': "(00) 00000-0000"}))
 
+#recebe o modelo e os campos que vão fazer parte do fomulário
     class Meta:
         model = Diarista
-        fields = '__all__'
+        exclude= ('codigo_ibge',)
     #limpar mascara dos fields
     def clean_cpf(self):
         cpf = self.cleaned_data['cpf']
-        #replace função do django que troca e limpar caracteres
+        #replace função do django que troca e limpa caracteres
         return cpf.replace('.','').replace('-','')
 
     def clean_cep(self):
