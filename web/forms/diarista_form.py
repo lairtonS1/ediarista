6cd6ef5728912db12c34b1d51059669efd8b757d
Lailson Da Silva <Lailsonkazumi@gmail.com>
2021-10-15 22:18:16 -0300

    foi feito a validação dos cep e o auto preenchimento dos campo codigo_ibge, atráves de uma api

--- web/forms/diarista_form.py
+++ web/forms/diarista_form.py
@@ -1,8 +1,11 @@
+import json
 from django import forms
 from ..models import Diarista
+from ..services import cep_service
 
 
 class DiaristaForm(forms.ModelForm):
+
     #aplicando mascara nos campos necessários
 
     cpf = forms.CharField(widget=forms.TextInput(attrs={'data-mask': "000.000.000-00"}))
@@ -10,10 +13,13 @@ class DiaristaForm(forms.ModelForm):
     telefone = forms.CharField(widget=forms.TextInput(attrs={'data-mask': "(00) 00000-0000"}))
 
 #recebe o modelo e os campos que vão fazer parte do fomulário
+
     class Meta:
         model = Diarista
         exclude= ('codigo_ibge',)
+
     #limpar mascara dos fields
+
     def clean_cpf(self):
         cpf = self.cleaned_data['cpf']
         #replace função do django que troca e limpa caracteres
@@ -21,9 +27,25 @@ class DiaristaForm(forms.ModelForm):
 
     def clean_cep(self):
         cep = self.cleaned_data['cep']
+        cep_formatado = cep.replace('-', '')
+        response = cep_service.buscar_cidade_cep(cep_formatado)
+        if response.status_code == 400:
+            raise forms.ValidationError("Cep informado é inválido")
+        cidade_api =json.loads(response.content)
+        if 'erro' in cidade_api:
+            raise forms.ValidationError("Cep informado não encontrado")
+
         return cep.replace('-', '')
 
     def clean_telefone(self):
         telefone = self.cleaned_data['telefone']
         return telefone.replace('(', '').replace(')', '').replace('-', '').replace(' ', '')
+
+    def save(self, commit=True):
+        instance = super(DiaristaForm, self).save(commit=False)
+        response = cep_service.buscar_cidade_cep(self.cleaned_data.get('cep'))
+        cidade_api = json.loads(response.content)
+        instance.codigo_ibge = cidade_api['ibge']
+        instance.save()
+        return instance
 
